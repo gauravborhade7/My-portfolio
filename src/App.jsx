7d3c866da996bf118a4938c976d7bf9ee30df12f
Gaurav Borhade <borhadegaurav0@gmail.com>
2025-03-15 23:18:10 +0530

    Fix Navbar import issue

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { About } from "./components/About/About";
 import { Contact } from "./components/Contact/Contact";
 import { Experience } from "./components/Experience/Experience";
 import { Hero } from "./components/Hero/Hero";
-import { Navbar } from "./components/Navbar/Navbar";
+import { Navbar } from "./components/Navbar/navbar.jsx";
 import { Skills } from "./components/skills/Skill.jsx";
 import {Projects} from './components/Projects/Projects.jsx'
 
